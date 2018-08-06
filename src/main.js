d7208e9e3ed98577a7d631ee639a07909ce8573a
kusiro <jackysoon871008@gmail.com>
2018-08-06 17:56:15 +0800

    Add some comment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -12,7 +11,7 @@ Object.keys(Components).forEach(key => {
   Vue.component(key, Components[key])
 })
 
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,

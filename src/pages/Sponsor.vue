d26b5c69d57b4383f71b4baf1ae85dd6a850a490
kusiro <jackysoon871008@gmail.com>
2018-08-08 13:55:49 +0800

    Add sponsor page

--- src/pages/Sponsor.vue
+++ src/pages/Sponsor.vue
@@ -1,9 +1,33 @@
 <template>
-  <article class="sponsor">
-    <section class="container">
-      <h1>Here in Sponsor Page</h1>
-    </section>
-  </article>
+    <div id="sponsor">
+        <div class="planet">
+            <img src="../assets/blue_planet.svg">
+        </div>
+        <div class="intro container">
+            <div class="row justify-content-end">
+                <div class="content col-md-5">
+                    <h1>合勤基金會</h1>
+                    <h3>企業介紹</h3>
+                    <p>合勤基金會以推動創新與創業為目標，深耕「創新思維」、「創業育成」與「友善環境」三大面向，持續關注「跨界創新」，與學生和青年創業家接軌，期以開拓的視野與無框架的創意思考激發突破自我的能量與火花。</p>
+                    <p>【鼓勵創新】舉辦「Innovation +」系列講座，邀請具有創新與創業實踐的創客蒞臨分享，傳達勇於創新與勇敢實踐的力量。</p>
+                    <p>【支持創業】與加速器中心、產、官、學、研合作，投入資源與支持物聯網相關提案的創新與創業，鼓勵產學合作、內部創業與青年創業。</p>
+                    <p>【培育優秀人才】提供資源鼓勵優秀學生逐夢踏實，贊助校園內之創新與創業相關競賽活動如Hackathon、搶鮮大賽等創新競賽活動，藉此培養跨界創新人才。</p>
+                    <p>【善盡社會責任】支持各種形式之公共論壇、創願演講、環保愛地球活動，鼓勵具關懷土地或愛護環境理念之實踐家，舉辦體驗活動關懷社會與人文發展。</p>
+                    <h3>目標</h3>
+                    <p>The next application revolution is here. In recent years, Wi-Fi access points have become more and more powerful devices. They pack powerful processors, lots of RAM and enough storage to turn this one-trick pony into a versatile application platform.</p>
+                    <p>Imagine combining router, NAS, smart home hub & security appliance into a single device. The place to provide centralized file services, security, Wi-Fi analytics or next generation location-based games, immediately available to any device connected to the router's Wi-Fi.</p>
+                    <p>We have built and API to tab into that power and make it available for 3rd parties to create the next wave of services. These APIs give developers access to: Wi-Fi, DNS and VPN configuration, real-time connection data and information about the connected Wi-Fi clients.</p>
+                    <p>At the hackathon, we expect participants to use the provided APIs and prototype a subscription service that can be marketed to families, small businesses or enterprises.</p>
+                    <h3>需具備技術＆能力</h3>
+                    <p>應用程序開發，Web編程和後端開發<br>App development, web programming and backend development</p>
+                    <h3>提供設備資源</h3>
+                    <p>Zyxel Armor Z2 NBG-6817 AC2600<br>雙頻無線 Gigabit 路由器</p>
+                    <h3>設備&資源詳細說明文件</h3>
+                    <p>另行提供下載連結給參賽隊伍</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
